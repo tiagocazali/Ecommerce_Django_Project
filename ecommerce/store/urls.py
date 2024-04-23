4b8638d2af4b2baf49189f36f2495bfc0615ea6a
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2024-04-23 02:33:47 +0200

    Add templates bases and link. Only Structure.

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
+    path('store/', views.store, name="store"),
+    path('shopping_cart/', views.shopping_cart, name="shopping_cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('profile/', views.profile, name="profile"),
+    path('login/', views.login, name="login"),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def store(request):
+    return render(request, 'store.html')
+
+def shopping_cart(request):
+    return render(request, 'shopping_cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def login(request):
+    return render(request, 'login.html')
+
