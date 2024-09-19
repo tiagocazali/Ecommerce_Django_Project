5e3096eaebcbf8daf944dbcf2dc02cfe3fd114b0
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2024-09-19 21:41:05 +0200

    FrontENd Store DOone

--- ecommerce/static/js/main.js
+++ ecommerce/static/js/main.js
@@ -2,6 +2,6 @@ var url = new URL(document.URL);
 var itens = document.getElementsByClassName("filter-orderby");
 
 for (i=0; i<itens.length; i++){
-    url.searchParams.set("order", itens[i].name);
-    itens[i].href = url.href
+    url.searchParams.set("order", itens[i].value);
+    itens[i].value = url.href;
 }
